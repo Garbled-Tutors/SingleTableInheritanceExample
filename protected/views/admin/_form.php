165c30f0f1cc1dddc4085cea542ae0e40d64a3b8
John Smith <private@unknown.com>
2013-02-04 10:12:09 -0800

    created new model and CRUD for admin

---
+++ protected/views/admin/_form.php
@@ -0,0 +1,42 @@
+<?php
+/* @var $this AdminController */
+/* @var $model Admin */
+/* @var $form CActiveForm */
+?>
+
+<div class="form">
+
+<?php $form=$this->beginWidget('CActiveForm', array(
+	'id'=>'admin-form',
+	'enableAjaxValidation'=>false,
+)); ?>
+
+	<p class="note">Fields with <span class="required">*</span> are required.</p>
+
+	<?php echo $form->errorSummary($model); ?>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'username'); ?>
+		<?php echo $form->textField($model,'username',array('size'=>60,'maxlength'=>100)); ?>
+		<?php echo $form->error($model,'username'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'role'); ?>
+		<?php echo $form->textField($model,'role',array('size'=>10,'maxlength'=>10)); ?>
+		<?php echo $form->error($model,'role'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'password_hash'); ?>
+		<?php echo $form->textField($model,'password_hash',array('size'=>60,'maxlength'=>60)); ?>
+		<?php echo $form->error($model,'password_hash'); ?>
+	</div>
+
+	<div class="row buttons">
+		<?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save'); ?>
+	</div>
+
+<?php $this->endWidget(); ?>
+
+</div><!-- form -->
